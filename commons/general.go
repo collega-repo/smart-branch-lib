0b01bc3e8e9dfff1e6c4d283e81319e4cb8f0945
gtxiqbal <gtxgtx@191919>
2023-12-02 20:46:54 +0700

    bug fix

--- commons/general.go
+++ commons/general.go
@@ -56,6 +56,7 @@ type datasource struct {
 type api struct {
 	Debug              bool       `yaml:"debug"`
 	CoreBankingService serviceApi `yaml:"coreBankingService"`
+	ExtService         serviceApi `yaml:"extService"`
 }
 
 type serviceApi struct {
